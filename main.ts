e00967f3f3c8bc720d6df18c0d30ac204aa160ca
Rob <rlmartain@hotmail.com>
2021-04-09 11:44:54 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+function InitLevels () {
+    list = []
+    CurrentLevelIndex = 0
+}
+function LoadLevel () {
+	
+}
+function InitSprites () {
+    list = []
+}
+function InitGame () {
+    InitLevels()
+    InitSprites()
+}
+let CurrentLevelIndex = 0
+let list: number[] = []
+InitGame()
