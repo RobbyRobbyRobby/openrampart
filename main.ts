8af951abe8b5599f065fd6dfcea21fe2de02218e
Rob <rlmartain@hotmail.com>
2021-04-09 15:18:49 +1000

    Begun adding enemy sprites

--- main.ts
+++ main.ts
@@ -9,6 +9,23 @@ function InitLevels () {
     Levels = []
     CurrentLevelIndex = 0
 }
+function CheckProjectiles () {
+    for (let projectileElement of Projectiles) {
+        if (Math.round(projectileElement.x) == Math.round(sprites.readDataNumber(projectileElement, "DestinationX")) && Math.round(projectileElement.y) == Math.round(sprites.readDataNumber(projectileElement, "DestinationY"))) {
+            for (let enemySpriteElement of EnemySprites) {
+                if (Math.round(projectileElement.x) == Math.round(enemySpriteElement.x) && Math.round(projectileElement.y) == Math.round(enemySpriteElement.y)) {
+                    music.bigCrash.play()
+                    enemySpriteElement.destroy(effects.fire, 500)
+                    EnemySprites.removeAt(EnemySprites.indexOf(enemySpriteElement))
+                }
+            }
+            projectileElement.setVelocity(0, 0)
+            projectileElement.destroy(effects.bubbles, 200)
+            music.thump.play()
+            Projectiles.removeAt(Projectiles.indexOf(projectileElement))
+        }
+    }
+}
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     Shoot()
 })
@@ -23,7 +40,29 @@ function LoadLevel () {
     LevelStart()
 }
 function LoadEnemySprites () {
-	
+    for (let index = 0; index <= 4; index++) {
+        newEnemy = sprites.create(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . e 2 2 . . . . . 
+            . . . . . . . . e . 2 2 . . . . 
+            . . . . . . . . e . 2 2 . . . . 
+            . . . . . . . . e . 2 2 . . . . 
+            . . . . . . . . e . 2 2 . . . . 
+            . . . . . . . . e . 2 2 . . . . 
+            . . . . . . . . e 2 2 . . . . . 
+            . . . . . . . . e 2 . . . . . . 
+            . . . . . . . . e . . . . . . . 
+            . e e e e e e e e e e e e e e e 
+            . e 4 4 4 4 4 4 4 4 4 4 4 4 e 1 
+            . e e 4 4 4 4 4 4 4 4 4 4 e 9 . 
+            . e e e e e e e e e e e e . 1 9 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, SpriteKind.Enemy)
+        newEnemy.follow(mySprite, EnemyBaseSpeed)
+        tiles.placeOnRandomTile(newEnemy, assets.tile`myTile`)
+        EnemySprites.push(newEnemy)
+    }
 }
 info.onCountdownEnd(function () {
 	
@@ -59,6 +98,11 @@ function InitGame () {
     InitLevels()
     LoadLevel()
 }
+function MoveEnemySprites () {
+    for (let enemySpriteElement of EnemySprites) {
+    	
+    }
+}
 function InitSpriteVariables () {
     SpriteTemplates = []
     EnemySprites = []
@@ -68,10 +112,10 @@ function InitSpriteVariables () {
     Cursor = sprites.create(assets.image`Crosshair`, SpriteKind.Player)
     ProjectileBaseSpeed = -25
     MaxActiveProjectiles = 3
+    EnemyBaseSpeed = 1
     InitPlayerSprite()
 }
 let NeutralSprites: number[] = []
-let EnemySprites: number[] = []
 let SpriteTemplates: number[] = []
 let newProjectile: Sprite = null
 let ProjectileVelocityY = 0
@@ -79,20 +123,17 @@ let ProjectileBaseSpeed = 0
 let ProjectileVelocityX = 0
 let ProjectileDestinationY = 0
 let ProjectileDestinationX = 0
-let Projectiles: Sprite[] = []
 let MaxActiveProjectiles = 0
+let EnemyBaseSpeed = 0
+let newEnemy: Sprite = null
 let Cursor: Sprite = null
 let mySprite: Sprite = null
+let EnemySprites: Sprite[] = []
+let Projectiles: Sprite[] = []
 let CurrentLevelIndex = 0
 let Levels: number[] = []
 InitGame()
 game.onUpdate(function () {
-    for (let value of Projectiles) {
-        if (Math.round(value.x) == Math.round(sprites.readDataNumber(value, "DestinationX")) && Math.round(value.y) == Math.round(sprites.readDataNumber(value, "DestinationY"))) {
-            value.setVelocity(0, 0)
-            value.destroy(effects.fire, 500)
-            music.smallCrash.play()
-            Projectiles.removeAt(Projectiles.indexOf(value))
-        }
-    }
+    CheckProjectiles()
+    MoveEnemySprites()
 })
